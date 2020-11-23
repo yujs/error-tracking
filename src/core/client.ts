e893a60c9917379f8034552da389f52ed87ad5f5
yujs <1062888640@qq.com>
2020-11-23 16:16:12 +0800

    feat: add core

---
+++ src/core/client.ts
@@ -0,0 +1,19 @@
+import { Options } from '../types/options';
+
+export class Client {
+  constructor(opt: Options) {
+      
+  }
+
+  send() {
+
+  }
+
+  catch() {
+
+  }
+}
+
+export function setup(options: Options) {
+  new Client(options);
+}
